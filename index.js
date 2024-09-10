add80d834305188919ae10244f1b3dda6bfa8dd2
pk <whiteexc@gmail.com>
2024-09-10 15:49:40 -0300

    Menu funcional

--- index.js
+++ index.js
@@ -1,28 +1,20 @@
-// let = meta = {
-//   value: "Ler um livro por Mês",
-//   checked: true,
-// };
 
-// let = metas = [
-//   meta,
-//     {
-//     value: "Caminhar 20 minutos",
-//     checked: false,
-//   },
-// ];
-
-// console.log(metas[1].value)
 
 const { select } = require("@inquirer/prompts");
 
 const start = async () => {
   while (true) {
+    
     const opcao = await select({
       message: "Menu > ",
       choices: [
         {
-          name: "Cadastrar Meta",
-          valeu: "cadastrar",
+          name: "Vamos Cadastrar",
+          value: "cadastrar",
+        },
+        {
+          name: "Listar Metas",
+          value: "listar",
         },
         {
           name: "Sair",
@@ -33,16 +25,16 @@ const start = async () => {
 
     switch (opcao) {
       case "cadastrar":
-        console.log("Vamos cadastrar")
-        break
+        console.log("Vamos cadastrar");
+        break;
 
       case "listar":
-        console.log("Vamos listar")
-        break
+        console.log("Vamos listar");
+        break;
 
       case "sair":
-        console.log("Até a proxima!")
-        return
+        console.log("Até a proxima!");
+        return;
     }
   }
 };
