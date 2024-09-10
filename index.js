38ccc5c506dc9a367380c7bd4c42a70f4718672b
pk <whiteexc@gmail.com>
2024-09-10 16:00:27 -0300

    cadastrar meta

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
+const { select, input } = require("@inquirer/prompts");
 
+let meta = {
+    value:'Tomar 3L de água por dia',
+    checked:false
+}
 
-const { select } = require("@inquirer/prompts");
+let metas = [ meta]
+
+const cadastrarMeta = async () => {
+    const meta = await input ({ message: "Digite a meta:"})
+    if(meta.length == 0 ){
+        console.log('A meta não pode ser vazia')
+        return
+    }
+
+    metas.push(
+        { value:meta, checked: false  }
+) 
+
+};
 
 const start = async () => {
   while (true) {
-    
     const opcao = await select({
       message: "Menu > ",
       choices: [
@@ -24,6 +41,11 @@ const start = async () => {
     });
 
     switch (opcao) {
+      case "cadastrar":
+        await cadastrarMeta()
+        console.log(metas);
+        break;
+
       case "cadastrar":
         console.log("Vamos cadastrar");
         break;
