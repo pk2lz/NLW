109216d131c36ee93886c8bb8c8bc2179fa8edc5
pk <whiteexc@gmail.com>
2024-09-10 16:13:26 -0300

    Listar metas

--- index.js
+++ index.js
@@ -1,23 +1,47 @@
-const { select, input } = require("@inquirer/prompts");
+const { select, input, checkbox } = require("@inquirer/prompts");
 
 let meta = {
-    value:'Tomar 3L de água por dia',
-    checked:false
-}
+  value: "Tomar 3L de água por dia",
+  checked: false,
+};
 
-let metas = [ meta]
+let metas = [meta];
 
 const cadastrarMeta = async () => {
-    const meta = await input ({ message: "Digite a meta:"})
-    if(meta.length == 0 ){
-        console.log('A meta não pode ser vazia')
-        return
-    }
+  const meta = await input({ message: "Digite a meta:" });
+  if (meta.length == 0) {
+    console.log("A meta não pode ser vazia");
+    return;
+  }
 
-    metas.push(
-        { value:meta, checked: false  }
-) 
+  metas.push({ value: meta, checked: false });
+};
 
+const listarMetas = async () => {
+  const respostas = await checkbox({
+    message:
+      "Use as setas para mudar de meta, o espaço para marcar ou desmarcar e o enter para finalizar essa etapa",
+    choices: [...metas],
+    instructions: false,
+  });
+  if (respostas.lenght == 0) {
+    console.log("Nenhuma meta selecionada.");
+    return;
+  }
+
+  metas.forEach((m) => {
+    m.checked = false;
+  });
+
+  respostas.forEach((resposta) => {
+    const meta = metas.find((m) => {
+      return m.value == resposta;
+    });
+
+    meta.checked = true;
+  });
+
+  console.log("Meta(s) marcadas como concluida(s)");
 };
 
 const start = async () => {
@@ -42,7 +66,7 @@ const start = async () => {
 
     switch (opcao) {
       case "cadastrar":
-        await cadastrarMeta()
+        await cadastrarMeta();
         console.log(metas);
         break;
 
@@ -51,7 +75,7 @@ const start = async () => {
         break;
 
       case "listar":
-        console.log("Vamos listar");
+        await listarMetas();
         break;
 
       case "sair":
